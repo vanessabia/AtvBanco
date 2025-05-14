867488d4992aa71a7be1543fc21ec78f84fe9ca8
Vanessa Beatriz <164911165+vanessabia@users.noreply.github.com>
2025-05-13 21:59:10 -0300

    Update Cliente.java

--- src/AtvHeranca/Cliente.java
+++ src/AtvHeranca/Cliente.java
@@ -5,14 +5,14 @@ import java.time.LocalDate;
  *
  * @author vanessa
  */
-public abstract class Cliente {
+public abstract class Cliente{
     protected LocalDate dataCadastro;
 
-    public Cliente(LocalDate dataCadastro) {
+    public Cliente(LocalDate dataCadastro){
         this.dataCadastro = dataCadastro;
     }
 
-    public LocalDate getDataCadastro() {
+    public LocalDate getDataCadastro(){
         return dataCadastro;
     }
     
